32b250704aef872e4bed7cee3f4f41dfd22e20f4
Benjamin Lind <benjamin.lind@northvolt.com>
2022-07-18 00:03:10 +0200

    Extract primes into a package to use in multiple modules

--- 27_quadratic_primes/27_quadratic_primes.go
+++ 27_quadratic_primes/27_quadratic_primes.go
@@ -1,25 +1,22 @@
 package main
 
 import (
+	"example.com/ben/primes"
 	"fmt"
-	"math"
 )
 
 func main() {
-	// Best a: -41, b: 461, n: 102, quad: 6683 was wrong
-
 	best := best{}
 
 	for a := -1000; a <= 1000; a++ {
-		for _, b := range getPrimes(1000) {
+		for _, b := range primes.GetPrimes(1000) {
 			n := 0
 			for {
 				quad := n*n + a*n + b
-				if !isPrime(quad) {
+				if !primes.IsPrime(quad) {
 					break
 				}
-				fmt.Printf("a: %v, b: %v, n: %v, quad: %v\n", a, b, n, quad)
-				// fmt.Println("is prime")
+				//fmt.Printf("a: %v, b: %v, n: %v, quad: %v\n", a, b, n, quad)
 
 				if n > best.n {
 					best.a = a
@@ -37,61 +34,4 @@ func main() {
 
 type best struct {
 	a, b, n, quad int
-}
-
-func getPrimes(n int) []int {
-	if n < 2 {
-		return []int{1}
-	}
-	primes := map[int]bool{}
-
-	numbers := make([]int, n) // [1, 2, 3, 4, 5, 6, 7, 8, 9...]
-	for i := 1; i <= n; i++ {
-		numbers[i-1] = i
-	}
-	// fmt.Println(primes)
-
-	for _, number := range numbers[1:] {
-		// fmt.Printf("Looking at %v: \n", number)
-		val, ok := primes[number]
-		if val == false && ok == true { // This has already been marked as a non-prime
-			continue
-		}
-		primes[number] = true
-		multiple := 2
-		for {
-			if multiple*number >= n {
-				break
-			} else {
-				primes[multiple*number] = false
-			}
-			multiple++
-		}
-	}
-
-	// fmt.Println(primes)
-	ps := []int{}
-	for key, val := range primes {
-		if val == true {
-			ps = append(ps, key)
-		}
-	}
-
-	// sort.Ints(ps)
-	return ps
-}
-
-func isPrime(n int) bool {
-	if n < 1 {
-		return false
-	}
-	// fmt.Printf("Checking if %v is a prime\n", n)
-	sqrtN := int(math.Floor(math.Sqrt(float64(n))))
-	// fmt.Println("sqrtN:", sqrtN)
-	for _, p := range getPrimes(sqrtN) { // [2, 3, 5, 7, 11...]
-		if n%p == 0 {
-			return false
-		}
-	}
-	return true
 }
---
+++ primes/primes.go
@@ -0,0 +1,61 @@
+package primes
+
+import "math"
+
+func IsPrime(n int) bool {
+	if n < 1 {
+		return false
+	}
+	// fmt.Printf("Checking if %v is a prime\n", n)
+	sqrtN := int(math.Floor(math.Sqrt(float64(n))))
+	// fmt.Println("sqrtN:", sqrtN)
+	for _, p := range GetPrimes(sqrtN) { // [2, 3, 5, 7, 11...]
+		if n%p == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+// GetPrimes generates a list of prime numbers that are less than or equal to n
+func GetPrimes(n int) []int {
+	if n < 2 {
+		return []int{1}
+	}
+	primes := map[int]bool{}
+
+	numbers := make([]int, n) // [1, 2, 3, 4, 5, 6, 7, 8, 9...]
+	for i := 1; i <= n; i++ {
+		numbers[i-1] = i
+	}
+	// fmt.Println(primes)
+
+	for _, number := range numbers[1:] {
+		// fmt.Printf("Looking at %v: \n", number)
+		val, ok := primes[number]
+		if val == false && ok == true { // This has already been marked as a non-prime
+			continue
+		}
+		primes[number] = true
+		multiple := 2
+		for {
+			if multiple*number >= n {
+				break
+			} else {
+				primes[multiple*number] = false
+			}
+			multiple++
+		}
+	}
+
+	// fmt.Println(primes)
+	ps := []int{}
+	for key, val := range primes {
+		if val == true {
+			ps = append(ps, key)
+		}
+	}
+
+	// sort.Ints(ps)
+	return ps
+}
