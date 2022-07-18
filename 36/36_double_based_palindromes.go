900e8c6823cccaea7dbab95ce26013d7f5fa6fda
Benjamin Lind <benjamin.lind@northvolt.com>
2022-07-18 10:25:57 +0200

    Add 36_double_based_palindromes.go

---
+++ 36/36_double_based_palindromes.go
@@ -0,0 +1,33 @@
+/*
+The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+(Please note that the palindromic number, in either base, may not include leading zeros.)
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	sumOfPalindromes := 0
+	for i := 1; i < 1000000; i++ {
+		b := fmt.Sprintf("%b", i)
+		d := fmt.Sprintf("%d", i)
+		//fmt.Println(d, b)
+		if isPalindrome(d) && isPalindrome(b) {
+			sumOfPalindromes += i
+			//fmt.Printf("Found double based palindrome: %d, %s\n", i, b)
+		}
+	}
+	fmt.Printf("The sum of the double based palindromes is %d", sumOfPalindromes)
+}
+
+func isPalindrome(number string) bool {
+	//fmt.Printf("verifying %s\n", number)
+	for i := 0; i < len(number)/2; i++ {
+		if string(number[i]) != string(number[len(number)-1-i]) {
+			return false
+		}
+	}
+	return true
+}
